8af8c259579dc9d0e92988ae4b0715afe7f0a908
Maria-Russier <russierduda@gmail.com>
2024-12-07 14:04:02 +0000

    adicionando comentários do dia 3

--- Dia 3/java.js
+++ Dia 3/java.js
@@ -4,6 +4,10 @@ let pesoDoUsuario = parseInit(prompt("Digite seu peso"))
 let alturaDoUsuario = parseFloat(prompt("Digite sua altura"))
 let anoDeNascimentodoUsuario = 2023 - idadeDoUsuario
 let imc = parseInt(pesoDoUsuario  /  (alturaDoUsuario  *  alturaDoUsuario))
+//+ - * %
+// 1 + 3 = 4 --> SUM
+// 1 + "3" = 13 --> CONCONATE
+// "1" + "3" --> CONCONATE 
 console.log("Olá " + nomeDoUsuario + ", você tem " + idadeDoUsuario + 
  " anos, nasceu em " + anoDeNascimentodoUsuario + ", tem " +
   alturaDoUsuario + " de altura, pesa " +
